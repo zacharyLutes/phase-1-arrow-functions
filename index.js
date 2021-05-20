718beb1265cf7aa0b93598dcb642873fd3df5046
forkingits1123 <lblack11235813@gmail.com>
2021-05-19 22:50:12 -0700

    commit tree e34fb672974f982f5e731402b2cf0c3e2ea2b943

---
+++ index.js
@@ -0,0 +1,19 @@
+const divide = (a=2000, b=100) => {
+    console.log("dividing inputs");
+    let x = a / b;
+    return x;
+}
+let v = 2000;
+let w = 100;
+
+
+
+const square = parameterOne => {
+    console.log("Squaring input");
+    let x = parameterOne ** 2;
+    return x
+}
+let x = 2;
+const result = square(x);
+
+const add = (parameterOne, parameterTwo) => parameterOne + parameterTwo;
